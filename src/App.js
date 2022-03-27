a3426ccf4c1e8ee3fc8e4c18d5b9db163acf3ca7
Tate Peñaranda <tate@tpenaranda.com>
2022-03-26 22:21:04 -0300

    Initial commit...

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App h-screen flex items-center justify-center">
+      <div className="text-gray-100 text-center">
+        <p className="text-6xl mb-3">Megma</p>
+        <p className="">Domótica para los amigos</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+     'lexend': ['Lexend', 'sans-serif'],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
